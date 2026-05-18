1df1a3f830c4aaf71b03d25864c43de205c3d194
AwaisSDev <mawais9171@gmail.com>
2026-05-18 23:35:03 +0500

    Added New Files

---
+++ api/config.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  // Allow cross-origin requests in case they fetch from different local test ports
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  res.status(200).json({
+    publicKey: process.env.VAPI_PUBLIC_KEY || '',
+    assistantId: process.env.VAPI_ASSISTANT_ID || ''
+  });
+}
